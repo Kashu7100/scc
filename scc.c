116baf752d6b143a07650f1ecbe175301de8730a
Kashu7100 <echo_six0566@yahoo.co.jp>
2021-06-25 04:50:50 -0500

    recursive descendent parser

--- scc.c
+++ scc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdarg.h>
+#include <assert.h>
 
 
 enum{
@@ -11,7 +13,7 @@ enum{
 
 
 typedef struct {
-  int key;
+  int type;
   int val;
   char *input; // token string
 } Token;
@@ -19,7 +21,6 @@ typedef struct {
 
 // store tokenized input to this array
 Token tokens[100];
-
 
 void tokenize(char *p){
   int i = 0;
@@ -30,7 +31,7 @@ void tokenize(char *p){
     }
     
     if(*p == '+' || *p == '-'){
-      tokens[i].key = *p;
+      tokens[i].type = *p;
       tokens[i].input = p;
       i++;
       p++;
@@ -38,7 +39,7 @@ void tokenize(char *p){
     }
     
     if(isdigit(*p)){
-      tokens[i].key = TOKEN_NUM;
+      tokens[i].type = TOKEN_NUM;
       tokens[i].input = p;
       tokens[i].val = strtol(p, &p, 10);
       i++;
@@ -49,13 +50,102 @@ void tokenize(char *p){
     exit(1);
   }
 
-  tokens[i].key = TOKEN_EOF;
+  tokens[i].type = TOKEN_EOF;
 }
 
 
-void error(int i){
-  fprintf(stderr, "[*] unexpected token: %s\n", tokens[i].input);
+void error(char *fmt, ...){
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
   exit(1);
+}
+
+
+int pos = 0;
+
+enum {
+  NODE_NUM = 256,
+};
+
+
+typedef struct node{
+  int type;
+  struct node *left;
+  struct node *right;
+  int val;
+} Node;
+
+
+Node *new_node(int op, Node *left, Node *right){
+  Node *node = malloc(sizeof(Node));
+  node->type = op;
+  node->left = left;
+  node->right = right;
+  return node;
+}
+
+
+Node *new_node_num(int val){
+  Node *node = malloc(sizeof(Node));
+  node->type = NODE_NUM;
+  node->val = val;
+  return node;
+}
+
+
+Node *number(){
+  if (tokens[pos].type == TOKEN_NUM)
+    return new_node_num(tokens[pos++].val);
+  error("[*] number expected, but got %s", tokens[pos].input);
+}
+
+
+Node *expr(){
+  Node *left = number();
+  for (;;){
+    int op = tokens[pos].type;
+    if (op != '+' && op != '-')
+      break;
+    pos++;
+    left = new_node(op, left, number());
+  }
+
+  if (tokens[pos].type != TOKEN_EOF)
+    error("[*] stray token: %s", tokens[pos].input);
+  return left;
+}
+
+
+char *regs[] = {
+  "rdi", "rsi", "r10", "r11", "r12", "r13", "r14", "r15", NULL
+};
+int cur=0;
+
+
+char *gen(Node *node) {
+  if (node->type == NODE_NUM) {
+    char *reg = regs[cur++];
+    if (!reg)
+      error("[*] register exhausted");
+    printf("  mov %s, %d\n", reg, node->val);
+    return reg;
+  }
+
+  char *dst = gen(node->left);
+  char *src = gen(node->right);
+
+  switch (node->type) {
+  case '+':
+    printf("  add %s, %s\n", dst, src);
+    return dst;
+  case '-':
+    printf("  sub %s, %s\n", dst, src);
+    return dst;
+  default:
+    assert(0 && "[*] unknown operator");
+  }
 }
 
 
@@ -66,35 +156,12 @@ int main(int argc, char **argv) {
   }
   
   tokenize(argv[1]);
+  Node* node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  
-  if(tokens[0].key != TOKEN_NUM)
-    error(0);
-  printf("  mov rax, %d\n", tokens[0].val);
- 
-  int i = 1;
-  while(tokens[i].key != TOKEN_EOF){
-    if(tokens[i].key == '+'){
-      i++;
-      if(tokens[i].key != TOKEN_NUM)
-        error(i);
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    if(tokens[i].key == '-'){
-      i++;
-      if(tokens[i].key != TOKEN_NUM)
-        error(i);
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    error(i);
-  }
+  printf("  mov rax, %s\n", gen(node));
   printf("  ret\n");
   return 0;
 }
